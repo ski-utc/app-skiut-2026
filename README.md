ed92399c619395fd41f57259777680b0076f971c
Mathis Delmaere <mathis.dlmr@gmail.com>
2024-09-16 20:48:38 +0200

    Commit initial

---
+++ README.md
@@ -0,0 +1,44 @@
+# Bienvenue sur l'app de Ski'UT 2025 en Expo
+
+## Pour commencer :
+1. Installer les dépendances
+
+   ```bash
+   npm install
+   ```
+   
+2. Lancer expo
+
+   ```bash
+    npx expo start
+   ```
+
+3. Lance le serveur Laravel (si j'ai fini de le setup)
+
+## Une fois expo lancé, 2 possibilités
+### 1. Lancer Expo sur son téléphone
+1. Installe Expo Go sur ton téléphone
+2. Appuie sur "s" dans ton terminale qui fait tourner Expo pour passer en mode expo go
+3. Scan le QR Code qui apparait dans ton terminal depuis l'app Expo Go sur ton Tél
+4. _(Optionnel) : Branche ton téléphone en USB pour que l'app se reload plus vite_
+
+### 2. Lancer un emulateur sur son PC
+1. Installe Android Studio 
+2. Créé un émulateur sur Andoid Studio ([Vidéo](https://youtu.be/JdQlicAP5W4?si=-o1wGceeZI8_Ob8j))
+3. Prie pour qu'il soit détecté par ton terminale
+
+
+## Quelques explications
+* app/  - Contient les fichiers de l'app
+* app.json  - C'est pour configurer l'app (nom, icônes, permissions)
+* assets/  - On y met les fichiers multimédia
+* babel.config.js  -  On touche pas (c'est pour le compilateur)
+* components/  - On y stocke des composants de page qu'on réutilise plusieurs fois (header, button, card...)
+* constants/  - On y stocke les variables globales (thème, dimensions, appels API)
+  * api/  - Contient les fichiers pour config et appeler l'API
+* expo-env.d.ts  - On touche pas, c'est pour Expo et l'IDE
+* hooks/  - On y stocke des fonctions pour gérer des états dans des composants (genre mettre le fond en noir quand on est en Dark Theme)
+* package.json  - Permet de stocker les dépendances pour npm install
+* package-lock.json  - On touche pas, c'est pour vérif la cohérence entre ton PC et package.json
+* ToDo.tkt  - C'est des trucs à faire pour plus tard
+* tsconfig.json  - On touche pas, c'est pour le compilateur JS
---
+++ constants/api/apiCalls.js
@@ -0,0 +1,81 @@
+import axios from "axios";
+import * as config from "./config";
+import * as SecureStore from "expo-secure-store";
+
+const bypass_login = true;
+
+// Token Refresh
+const refreshTokens = async () => {
+  const refreshtoken = await SecureStore.getItemAsync("refreshToken");
+  if (!refreshtoken) {
+    return null;
+  }
+  const apiConfig = { headers: { Authorization: `Bearer ${refreshtoken}` } };
+  try {
+    const res = await axios.get(`${config.API_BASE_URL}/auth/refresh`, apiConfig);
+    await SecureStore.setItemAsync("accessToken", res.data.data.access_token);
+    await SecureStore.setItemAsync("refreshToken", res.data.data.refresh_token);
+    return true;
+  } catch (e) {
+    await SecureStore.setItemAsync("refreshToken", "");
+    await SecureStore.setItemAsync("accessToken", "");
+    throw new Error("JWT expired");
+  }
+};
+
+// apiGet Function with JWT authentication
+export const apiGet = async (url) => {
+  let response;
+  let accessToken = await SecureStore.getItemAsync("accessToken");
+  let apiConfig = { headers: { Authorization: `Bearer ${accessToken}` } };
+
+  try {
+    if (bypass_login) {
+      console.log(`Bypassing login on ${config.API_BASE_URL}/${url}`);
+      response = await axios.get(`${config.API_BASE_URL}/${url}`, { headers: {} });
+    } else {
+      response = await axios.get(`${config.API_BASE_URL}/${url}`, apiConfig);
+    }
+  } catch (error) {
+    if (error.response?.data?.JWT_ERROR) {
+      console.error("JWT expired, attempting to refresh tokens...");
+      await refreshTokens();
+      return await apiGet(url); // Retry with new token
+    } else {
+      throw new Error(error.response?.data?.message || "RequestError");
+    }
+  }
+
+  return response.data;
+};
+
+// apiPost Function with JWT authentication and multimedia option
+export const apiPost = async (url, data, multimedia = false) => {
+  let response;
+  let accessToken = await SecureStore.getItemAsync("accessToken");
+  let apiConfig = {
+    headers: multimedia
+      ? { Authorization: `Bearer ${accessToken}`, "content-type": "multipart/form-data" }
+      : { Authorization: `Bearer ${accessToken}` },
+  };
+
+  try {
+    response = await axios.post(`${config.API_BASE_URL}/${url}`, data, apiConfig);
+  } catch (error) {
+    if (error.response?.data?.JWT_ERROR) {
+      console.error("JWT expired, attempting to refresh tokens...");
+      await refreshTokens();
+      return await apiPost(url, data, multimedia); // Retry with new token
+    } else {
+      throw new Error(error.response?.data?.message || "RequestError");
+    }
+  }
+
+  return response.data;
+};
+
+// Public GET request without JWT authentication
+export const apiGetPublic = async (url) => {
+  const response = await axios.get(`${config.API_BASE_URL}/${url}`);
+  return response.data;
+};

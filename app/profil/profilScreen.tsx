73453b6b95dfc8b5810e356b16da6a599d0d6e6c
Mathis Delmaere <mathis.delmaere@free.fr>
2024-12-20 20:15:29 +0100

    Débug et fin du rendu du drawer (surtout la partie haute qui n'était pas encore faite)

--- app/profil/profilScreen.tsx
+++ app/profil/profilScreen.tsx
@@ -1,16 +1,62 @@
 import { View, StyleSheet } from "react-native";
-import { Text } from "react-native";
+import { Text, Image } from "react-native";
 import Header from "../../components/header";
 import React from 'react';
 import BoutonProfil from "../../components/profil/boutonProfil";
+import { Fonts, Colors } from "@/constants/GraphSettings";
 
 
 export default function Profil() {
     return (
-        <View style={styles.container}>
-            <Header  />
-            <View style={styles.headerContainer}>
-                <Text>John Doe</Text>
+        <View 
+            style={{
+                height: "100%",
+                width: "100%",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "start",
+                backgroundColor: "white",
+                paddingBottom: 8,
+            }}
+        >
+            <Header/>
+            <View
+                style={{
+                    flexDirection: 'row',
+                    width: '100%',
+                    justifyContent: 'start',
+                    alignItems: 'center',
+                    paddingBottom: 8,
+                    paddingHorizontal: 20,
+                    gap: 25
+                }}
+            >
+                <Image source={require("../../assets/images/OursCabine.png")} style={{height: '164px', width: '164px'}} />
+                <View style={{
+
+                    }}
+                >
+                    <Text
+                        style={{
+                            fontSize: 32,
+                            fontFamily: Fonts.Inter.Basic,
+                            fontWeight: '600',
+                        }}
+                    >
+                        John Doe
+                    </Text>
+                    <Text
+                        style={{
+                            fontSize: 20,
+                            fontFamily: Fonts.Inter.Basic,
+                            fontWeight: '400',
+                            color: Colors.gray,
+                        }}
+                    >
+                        Chambre 112
+                    </Text>
+                </View>
             </View>
             <View style={styles.navigationContainer}>
                 <BoutonProfil 
@@ -22,7 +68,6 @@ export default function Profil() {
                      }}
                 />
             </View>
-
             <View style={styles.navigationContainer}>
                 <BoutonProfil 
                     nextRoute={"StopVssScreen"} 
@@ -94,23 +139,6 @@ export default function Profil() {
 }
 
 const styles = StyleSheet.create({
-    container: {
-        height: "100%",
-        width: "100%",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: "white",
-        paddingBottom: 8,
-    },
-    headerContainer: {
-        width: '100%',
-        flex: 1,
-        backgroundColor: 'white',
-        paddingHorizontal: 20,
-        paddingBottom: 16,
-    },
     navigationContainer: {
         flexDirection: 'row',
         justifyContent: 'center',

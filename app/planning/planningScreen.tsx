ce370f10da130683cc206a4790a49d6cf7a22e84
lcaignae <louise.caignaert@etu.utc.fr>
2025-01-11 17:01:07 +0100

    planning modifié pour ajouter mention payant / inclus

--- app/planning/planningScreen.tsx
+++ app/planning/planningScreen.tsx
@@ -41,15 +41,15 @@ export default function PlanningScreen() {
       }
     } catch (error) {
       if (error.message === 'NoRefreshTokenError' || error.JWT_ERROR) {
-        setUser(null); 
+        setUser(null);
       } else {
         setError(error.message || "Une erreur inattendue est survenue");
       }
     } finally {
       setLoading(false);
       setTimeout(() => {
-        setDisableRefresh(false); 
-      }, 5000); 
+        setDisableRefresh(false);
+      }, 5000);
     }
   };
 
@@ -57,9 +57,9 @@ export default function PlanningScreen() {
     const today = new Date();
     const formatter = new Intl.DateTimeFormat('fr-FR', { day: '2-digit', month: '2-digit', year: 'numeric' });
     const [{ value: day }, , { value: month }, , { value: year }] = formatter.formatToParts(today);
-    return `${year}-${month}-${day}`; 
+    return `${year}-${month}-${day}`;
   };
-  
+
   useEffect(() => {
     const loadAsyncFonts = async () => {
       await loadFonts();
@@ -88,32 +88,32 @@ export default function PlanningScreen() {
     if (item.type === "days") {
       return (
         <View style={{
-            flexDirection: "row",
-            justifyContent: "space-around",
-            width: "95%",
-            marginTop: 5,
-            height: 60,
-            borderRadius: 12,
-            borderWidth: 2,
-            borderColor: Colors.orange,
-            alignSelf: "center",
+          flexDirection: "row",
+          justifyContent: "space-around",
+          width: "95%",
+          marginTop: 5,
+          height: 60,
+          borderRadius: 12,
+          borderWidth: 2,
+          borderColor: Colors.orange,
+          alignSelf: "center",
         }}>
           {item.days.map((day: string, index: number) => {
             const [letter, number] = day.split(" ");
             const isSelected = selectedDate === `2025-01-${number}`;
-  
+
             return (
               <TouchableOpacity
                 key={index}
                 style={{
-                    flex: 1,
-                    paddingTop: 8,
-                    paddingBottom: 8,
-                    borderRadius: 8,
-                    flexDirection: "column",
-                    justifyContent: "flex-start",
-                    alignItems: "center",
-                    backgroundColor: isSelected ? Colors.orange : Colors.white
+                  flex: 1,
+                  paddingTop: 8,
+                  paddingBottom: 8,
+                  borderRadius: 8,
+                  flexDirection: "column",
+                  justifyContent: "flex-start",
+                  alignItems: "center",
+                  backgroundColor: isSelected ? Colors.orange : Colors.white
                 }}
                 onPress={() => handlePress(letter as keyof typeof dayMap, number)}
               >
@@ -129,91 +129,95 @@ export default function PlanningScreen() {
         </View>
       );
     } else if (item.type === "activities") {
-        const formattedDate0 = selectedDate
+      const formattedDate0 = selectedDate
         ? new Intl.DateTimeFormat('fr-FR', { weekday: 'long', day: 'numeric', month: 'long' }).format(new Date(selectedDate))
         : "Sélectionnez une date";
-      
+
       const formattedDate = formattedDate0.charAt(0).toUpperCase() + formattedDate0.slice(1);
 
       return (
         <>
-        <Text style={{
+          <Text style={styles.infoText}>
+            Les animations déjà réservées sont indiquées en{" "}
+            <Text style={styles.orangeText}>orange</Text>.
+            Vas-y seulement si tu as pris ta place !
+          </Text>
+
+          <Text style={{
             fontFamily: Fonts.Title.Light,
             fontSize: 16,
             fontWeight: "600",
             color: Colors.black,
             marginTop: 20,
-        }}>
+          }}>
             {formattedDate || "Sélectionnez une date"}
           </Text>
           {item.activities.length > 0 ? (
             item.activities.map((activity: Activity, index: number) => {
+              const titleColor = activity.payant ? Colors.orange : Colors.black; // Définir la couleur en fonction de 'payant'
               return (
-                <View 
-                    key={index}
-                    style={{
-                        padding: 12,
-                        flexDirection: 'row',
-                        justifyContent: 'flex-start',
-                        alignItems: 'center',
-                        borderBottomColor: Colors.customGray,
-                        borderBottomWidth: 1,
-                        opacity: (activity.status === "past") ? 0.4 : 1
-                    }}
+                <View
+                  key={index}
+                  style={{
+                    padding: 12,
+                    flexDirection: 'row',
+                    justifyContent: 'flex-start',
+                    alignItems: 'center',
+                    borderBottomColor: Colors.customGray,
+                    borderBottomWidth: 1,
+                    opacity: (activity.status === "past") ? 0.4 : 1
+                  }}
                 >
-                    <View
-                        style={{
-                            height: 9,
-                            width: 9,
-                            marginRight: 10,
-                            justifyContent: 'center',
-                            alignItems: 'center'
-                        }}
+                  <View
+                    style={{
+                      height: 9,
+                      width: 9,
+                      marginRight: 10,
+                      justifyContent: 'center',
+                      alignItems: 'center'
+                    }}
+                  >
+                    <View style={{ height: '100%', width: '100%', borderRadius: 61, backgroundColor: (activity.status === "current") ? 'green' : 'white' }}></View>
+                  </View>
+                  <View
+                    style={{
+                      justifyContent: 'center',
+                      alignItems: 'flex-start',
+                      gap: 8,
+                      flexDirection: 'column'
+                    }}
+                  >
+                    <Text
+                      style={{
+                        fontFamily: Fonts.Text.Bold,
+                        fontWeight: '600',
+                        fontSize: 16,
+                        color: titleColor,
+                      }}
                     >
-                        <View style={{height: '100%', width: '100%', borderRadius: 61, backgroundColor: (activity.status === "current") ? 'green' : 'white'}}></View>
-                    </View>
-                    <View
-                        style={{
-                            justifyContent: 'center',
-                            alignItems: 'flex-start',
-                            gap: 8,
-                            flexDirection: 'column'
-                        }}
+                      {activity.activity}
+                    </Text>
+                    <Text
+                      style={{
+                        fontFamily: Fonts.Text.Bold,
+                        fontWeight: '600',
+                        fontSize: 14,
+                        color: Colors.gray
+                      }}
                     >
-                        <Text 
-                            style={{
-                                fontFamily: Fonts.Text.Bold,
-                                fontWeight: '600',
-                                fontSize: 16,
-                                color: Colors.black
-                            }}
-                        >
-                            {activity.time.start} - {activity.time.end}
-                        </Text>
-                        <Text 
-                            style={{
-                                fontFamily: Fonts.Text.Bold,
-                                fontWeight: '600',
-                                fontSize: 14,
-                                color: Colors.gray
-                            }}
-                        >
-                            {activity.activity}
-                        </Text>
-                    </View>
+
+                      {activity.time.start} - {activity.time.end}
+                    </Text>
+                  </View>
                 </View>
               );
             })
           ) : (
-            <Text 
-                style={{
-                    fontSize: 16,
-                    color: Colors.gray,
-                    textAlign: "center",
-                    marginTop: 10,
-                }}>
-              Aucune activité disponible pour cette date.
-            </Text>
+            <View style={styles.noActivitiesContainer}>
+              <Text style={styles.noActivitiesText}>
+                Sélectionne une date pour voir les activités prévues.
+              </Text>
+            </View>
           )}
         </>
       );
@@ -221,10 +225,8 @@ export default function PlanningScreen() {
     return null;
   };
 
-  if(error!='') {
-    return(
-      <ErrorScreen error={error}/>
-    )
+  if (error) {
+    return <ErrorScreen error={error} />;
   }
 
   if (loading) {
@@ -257,50 +259,79 @@ export default function PlanningScreen() {
 
   return (
     <View style={styles.container}>
-      <Header refreshFunction={fetchPlanning} disableRefresh={disableRefresh}/>
-      <View style={styles.content}>
-        <BoutonRetour previousRoute={"homeNavigator"} title={"Planning"} />     
-        <View style={{
-            height: "100%",
-            width: "100%",
-            backgroundColor: Colors.white,
-          }}>
-            <FlatList
-              data={data}
-              keyExtractor={(item, index) => `${item.type}-${index}`}
-              renderItem={renderItem}
-            />
-        </View>
+      <Header refreshFunction={fetchPlanning} disableRefresh={disableRefresh} />
+      <View style={styles.headerContainer}>
+        <BoutonRetour previousRoute="homeScreen" title="Planning" />
       </View>
+      <FlatList
+        data={data}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={renderItem}
+        contentContainerStyle={styles.listContainer}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    height: "100%",
-    width: "100%",
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: Colors.white,
-  },
-  content: {
-    width: '100%',
     flex: 1,
     backgroundColor: Colors.white,
+  },
+  headerContainer: {
+    width: '100%',
     paddingHorizontal: 20,
-    paddingBottom: 16,
+    paddingBottom: 8,
+  },
+  listContainer: {
+    paddingHorizontal: 20,
+  },
+  loadingContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   dayLetter: {
-    fontSize: 12,
-    fontFamily: "Inter",
-    fontWeight: "500",
+    fontSize: 14,
+    fontWeight: '600',
+    fontFamily: Fonts.Text.Medium,
+    paddingBottom: 6,
   },
   dayNumber: {
     fontSize: 16,
-    fontFamily: "Inter",
-    fontWeight: "600",
-  }, 
+    fontWeight: '700',
+    fontFamily: Fonts.Text.Bold,
+  },
+  noActivitiesContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  noActivitiesText: {
+    fontFamily: Fonts.Text.Medium,
+    fontSize: 18,
+    textAlign: 'center',
+    color: Colors.gray,
+  },
+  infoText: {
+    fontFamily: Fonts.Text.Light,
+    fontSize: 14,
+    color: Colors.black, // Default color for the whole text
+    marginTop: 16,
+    textAlign: 'center',
+    zIndex: 1,
+  },
+  orangeText: {
+    fontFamily: Fonts.Text.Light,
+    fontSize: 14,
+    color: Colors.orange, // Only the word "orange" will be in orange
+  },
+  subInfoText: {
+    fontFamily: Fonts.Text.Light,
+    fontSize: 14,
+    color: Colors.black,
+    textAlign: 'center',
+    zIndex: 1,
+  },
+  
 });

d0d1ca6c4ad9894caf975b80dae6beec0e78c2e8
Eric Bjarstal <ericbmousy@gmail.com>
2025-01-11 17:35:14 +0100

    fix call header

--- app/home/homeScreen.tsx
+++ app/home/homeScreen.tsx
@@ -73,7 +73,7 @@ export default function HomeScreen() {
           justifyContent: 'center',
         }}
       >
-        <Header/>
+        <Header refreshFunction={undefined} disableRefresh={undefined}/>
         <View
           style={{
             width: '100%',
